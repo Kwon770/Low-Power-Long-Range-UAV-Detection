8684b91598896b2573555619d868f96e7feea0a0
Kwon770 <sckwon770@gmail.com>
2023-01-23 09:47:46 -0500

    Init

---
+++ sketch_jan22a/sketch_jan22a.ino
@@ -0,0 +1,14 @@
+#include <RadioHead.h>
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+ Serial.write("123");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
